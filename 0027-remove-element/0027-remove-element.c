d53d6d77256b2808e8ae41fddaf4ae9db3fe7bb6
Tran Trung Dung <trantrungdung.work@gmail.com>
2026-07-25 22:06:55 +0700

    Time: 0 ms (100%), Space: 10.5 MB (28.33%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,11 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int slow = 0;
+
+    for(int fast = 0; fast < numsSize; fast++){
+        if(nums[fast] != val){
+            nums[slow++] = nums[fast];
+        }
+    }
+
+    return slow;
+}
