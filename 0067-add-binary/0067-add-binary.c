9af0b4365afbfbb38b80b13e1cca237d900fb832
Tran Trung Dung <trantrungdung.work@gmail.com>
2026-07-27 22:25:47 +0700

    Time: 0 ms (100%), Space: 8.6 MB (99.29%) - LeetHub

--- 0067-add-binary/0067-add-binary.c
+++ 0067-add-binary/0067-add-binary.c
@@ -1,27 +1,31 @@
 char* addBinary(char* a, char* b) {
-    int len_a = strlen(a) - 1;
-    int len_b = strlen(b) - 1;
-    
-    int carry = 0;
+    int lenA = strlen(a);
+    int lenB = strlen(b);
 
-    int size = ((len_a + 1) > (len_b + 1)) ? (len_a + 1) : (len_b + 1);
+    int i = lenA - 1;
+    int j = lenB - 1;
+    
+    int size = ((i + 1) > (j + 1)) ? (i + 1) : (j + 1);
+
     char *ans = (char*)malloc((size + 2) * sizeof(char));
     int k = size + 1;
     ans[k--] = '\0';
+    
+    int carry = 0;
 
-    while(len_a >= 0 || len_b >= 0 || carry != 0){
+    while(i >= 0 || j >= 0 || carry != 0){
         int sum = carry; //a[len_a] + b[len_a] + carry
 
-        if(len_a >= 0)
-            sum += a[len_a] - '0';
+        if(i >= 0)
+            sum += a[i] - '0';
 
-        if(len_b >= 0)
-            sum += b[len_b] - '0';
+        if(j >= 0)
+            sum += b[j] - '0';
 
         ans[k] = (sum % 2) + '0';
         carry = sum / 2;
-        len_a--;
-        len_b--;
+        i--;
+        j--;
         k--;
     }
     if(k >= 0)
